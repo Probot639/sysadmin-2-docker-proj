3783acac318cd472f7cad936a5bd731bf039be77
0xd355a <odessarybski@gmail.com>
2025-11-27 23:01:56 -0500

    demonstrating that the offline worker has no networking capabilities

--- dfile-test/offline-worker/process-logs.sh
+++ dfile-test/offline-worker/process-logs.sh
@@ -3,6 +3,9 @@ echo "[offline-worker] Starting with no network..."
 echo "[offline-worker] Listing /logs contents:"
 ls -l /logs || echo "No /logs directory mounted."
 
+echo "[offline-worker] running 'ifconfig' to show networks"
+ifconfig
+
 echo "[offline-worker] Sleeping for an hour so you can inspect me."
 sleep 3600
 
